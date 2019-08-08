b41c41fcef376e7c101848dc3f88b7f0258819b9
ayulockin <mein2work@gmail.com>
2019-08-08 08:33:23 +0530

    Code cleaned

--- ssdMobileNetApproach/main.py
+++ ssdMobileNetApproach/main.py
@@ -29,36 +29,28 @@ def id_class_name(class_id, classes):
 # Loading model
 model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
                                       'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
-# image = cv2.imread("image.jpeg")
 
 vs = cv2.VideoCapture(0)
+
 while True:
-        _, frame = vs.read()
-        image_height, image_width, _ = frame.shape
+	_, frame = vs.read()
+    image_height, image_width, _ = frame.shape
 
-        model.setInput(cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True))
-        output = model.forward()
-        # print(output[0,0,:,:].shape)
+    model.setInput(cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True))
+    output = model.forward()
+
+    for detection in output[0, 0, :, :]:
+    	confidence = detection[2]
+    	if confidence > .5:
+        class_id = detection[1]
+        class_name=id_class_name(class_id,classNames)
+        if class_name == 'person':
+        	print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
 
 
-        for detection in output[0, 0, :, :]:
-            confidence = detection[2]
-            if confidence > .5:
-                class_id = detection[1]
-                class_name=id_class_name(class_id,classNames)
-                if class_name == 'person':
-                  print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
-                # box_x = detection[3] * image_width
-                # box_y = detection[4] * image_height
-                # box_width = detection[5] * image_width
-                # box_height = detection[6] * image_height
-                # cv2.rectangle(frame, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
-                # cv2.putText(frame,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.005*image_width),(0, 0, 255))
+    cv2.imshow('image', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        cv2.imshow('image', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
-# cv2.imwrite("image_box_text.jpg",image)
-
-cv2.waitKey(0)
+vs.release()
 cv2.destroyAllWindows()
